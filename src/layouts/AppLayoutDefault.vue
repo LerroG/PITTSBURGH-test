a27b1ab35032b522e782f0f596ba37b4b4bb8740
Ruslan <ssakoks@mail.ru>
2025-02-09 00:42:25 +0500

    add text and colors css

--- src/layouts/AppLayoutDefault.vue
+++ src/layouts/AppLayoutDefault.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Header from './default/Header.vue'
+import Footer from './default/Footer.vue'
 </script>
 
 <template>
-	<div class="min-h-screen w-screen font-poppins">
+	<div class="min-h-screen w-screen font-poppins flex flex-col">
 		<Header />
-		<main>
+		<main class="bg-gray-100 flex-1">
 			<slot />
 		</main>
+		<Footer />
 	</div>
 </template>
---
+++ src/layouts/default/Footer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+	<footer class="bg-black h-[440px]">
+		<div>
+			<div>
+				<div class="font-inter font-bold text-2xl text-white">Exclusive</div>
+				<div class="text-[20px] text-white font-medium">Subscribe</div>
+				<div class="heading-110PX-bold text-white">
+					Get 10% off your first order
+				</div>
+				<div class="heading-110PX-semibold text-white font-">
+					Get 10% off your first order
+				</div>
+				<div class="heading-110PX-medium text-white">
+					Get 10% off your first order
+				</div>
+				<div class="heading-110PX-regular text-white">
+					Get 10% off your first order
+				</div>
+			</div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+		<div></div>
+	</footer>
+</template>
